630309f897ce34f2f270b8d657d3e5c509d74268
eceozaydin <125249726+eceozaydin@users.noreply.github.com>
2023-11-08 14:23:43 +0300

    Update server ip changed as localhost on environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production:false,
   keycloak:{
-    issuer:"http://10.34.79.80:8080/realms/angular",
+    issuer:"http://127.0.0.1:8080/realms/angular",
     redirectUri: "http://localhost:4200",
     clientId:"sso-app",
     scope:"openid profile offline_access",
